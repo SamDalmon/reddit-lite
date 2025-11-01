2558f3e6e06b1e8c0a4399cf835320d903d4c1c7
SamDalmon <seven8mine@yahoo.co.nz>
2025-11-01 16:17:56 +1300

    added header and post list components

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,11 @@
 import React from "react";
+import redditLogo from "../reddit-logo.png";
 
 export default function Header(){
   return (
     <>
+      <img src={redditLogo} alt="Reddit Logo"></img>
+      <h1>Reddit</h1>
     </>
   )
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,10 @@
 import React from "react";
+import PostList from "../Features/Posts/PostList";
 
 export default function Home(){
   return (
-    <></>
+    <>
+      <PostList/>
+    </>
   )
 } 
