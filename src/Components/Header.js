9dc8d50c304cfffcec4b8208cded20196a0ba808
SamDalmon <seven8mine@yahoo.co.nz>
2025-11-04 17:42:28 +1300

    got reddit logo and title displaying side by side

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,10 +2,15 @@ import React from "react";
 import redditLogo from "../reddit-logo.png";
 
 export default function Header(){
+  const imageStyle = {
+    width: '100px',
+    height: '100px'
+  }
+  
   return (
-    <>
-      <img src={redditLogo} alt="Reddit Logo"></img>
+    <div style={{display: 'flex'}}>
+      <img style={imageStyle} src={redditLogo} alt="Reddit Logo"></img>
       <h1>Reddit</h1>
-    </>
+    </div>
   )
 }
